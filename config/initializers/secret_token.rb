f8c9b6347e6f2340f5c4bbff0bd3c169c69b0f75
Ednilson Vizotto Martins <e.vizotto.martins@outlook.com>
2014-09-12 15:19:46 -0300

    initial push test to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesafioProgramacao1::Application.config.secret_key_base = 'bb2869f68ee86cf7a4bdd4c6fc32474de7411083d3b404a1cd0b4cbc7af0505f5c7999dfebcdbe5de1c28de7a38c002374701597c57d537ff8c643bd45425a2f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DesafioProgramacao1::Application.config.session_store :cookie_store, key: '_desafio-programacao-1_session'
